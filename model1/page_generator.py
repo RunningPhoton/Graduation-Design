cb4de234c26cc2bdfabc73102a16551280c4124e
Your Name <you@example.com>
2018-04-19 10:21:53 +0800

    generator text

--- model1/page_generator.py
+++ model1/page_generator.py
@@ -1,16 +1,61 @@
+# coding: utf-8
+
+# # 《安娜卡列尼娜》新编——利用TensorFlow构建LSTM模型
+# 
+# 最近看完了LSTM的一些外文资料，主要参考了Colah的blog以及Andrej Karpathy blog的一些关于RNN的材料，准备动手去实现一个LSTM模型。代码的基础框架来自于Udacity上深度学习纳米学位的课程（付费课程）的一个demo，我刚开始看代码的时候真的是一头雾水，很多东西没有理解，后来反复查阅资料，并我重新对代码进行了学习和修改，对步骤进行了进一步的剖析，下面将一步步用TensorFlow来构建LSTM模型进行文本学习并试图去生成新的文本。
+# 
+# 关于RNN与LSTM模型本文不做介绍，详情去查阅资料过着去看上面的blog链接，讲的很清楚啦。这篇文章主要是偏向实战，来自己动手构建LSTM模型。
+# 
+# 数据集来自于外文版《安娜卡列妮娜》书籍的文本文档（本文后面会提供整个project的git链接）。
+
+# In[1]:
+
 import time
 from collections import namedtuple
 
 import numpy as np
 import tensorflow as tf
 
+# # 1 数据加载与预处理
+
+# In[2]:
+
 with open('anna.txt', 'r') as f:
-    text=f.read()
+    text = f.read()
 vocab = set(text)
 vocab_to_int = {c: i for i, c in enumerate(vocab)}
 int_to_vocab = dict(enumerate(vocab))
 encoded = np.array([vocab_to_int[c] for c in text], dtype=np.int32)
 
+# In[3]:
+
+text[:100]
+
+# In[4]:
+
+encoded[:100]
+
+# In[5]:
+
+len(vocab)
+
+
+# # 2 分割mini-batch
+# 
+# 
+# 
+# <img src="assets/sequence_batching@1x.png" width=500px>
+# 
+# 
+# 完成了前面的数据预处理操作，接下来就是要划分我们的数据集，在这里我们使用mini-batch来进行模型训练，那么我们要如何划分数据集呢？在进行mini-batch划分之前，我们先来了解几个概念。
+# 
+# 假如我们目前手里有一个序列1-12，我们接下来以这个序列为例来说明划分mini-batch中的几个概念。首先我们回顾一下，在DNN和CNN中，我们都会将数据分batch输入给神经网络，加入我们有100个样本，如果设置我们的batch_size=10，那么意味着每次我们都会向神经网络输入10个样本进行训练调整参数。同样的，在LSTM中，batch_size意味着每次向网络输入多少个样本，在上图中，当我们设置batch_size=2时，我们会将整个序列划分为6个batch，每个batch中有两个数字。
+# 
+# 然而由于RNN中存在着“记忆”，也就是循环。事实上一个循环神经网络能够被看做是多个相同神经网络的叠加，在这个系统中，每一个网络都会传递信息给下一个。上面的图中，我们可以看到整个RNN网络由三个相同的神经网络单元叠加起来的序列。那么在这里就有了第二个概念sequence_length（也叫steps），中文叫序列长度。上图中序列长度是3，可以看到将三个字符作为了一个序列。
+# 
+# 有了上面两个概念，我们来规范一下后面的定义。我们定义一个batch中的序列个数为N（batch_size），定义单个序列长度为M（也就是我们的steps）。那么实际上我们每个batch是一个N x M的数组。在这里我们重新定义batch_size为一个N x M的数组，而不是batch中序列的个数。在上图中，当我们设置N=2， M=3时，我们可以得到每个batch的大小为2 x 3 = 6个字符，整个序列可以被分割成12 / 6 = 2个batch。
+
+# In[6]:
 
 def get_batches(arr, n_seqs, n_steps):
     '''
@@ -38,6 +83,26 @@ def get_batches(arr, n_seqs, n_steps):
         yield x, y
 
 
+# 上面的代码定义了一个generator，调用函数会返回一个generator对象，我们可以获取一个batch。
+
+# In[7]:
+
+batches = get_batches(encoded, 10, 50)
+x, y = next(batches)
+
+# In[8]:
+
+print('x\n', x[:10, :10])
+print('\ny\n', y[:10, :10])
+
+
+# # 3 模型构建
+# 模型构建部分主要包括了输入层，LSTM层，输出层，loss，optimizer等部分的构建，我们将一块一块来进行实现。
+
+# ## 3.1 输入层
+
+# In[9]:
+
 def build_inputs(num_seqs, num_steps):
     '''
     构建输入层
@@ -54,8 +119,12 @@ def build_inputs(num_seqs, num_steps):
     return inputs, targets, keep_prob
 
 
+# ## 3.2 LSTM层
+
+# In[10]:
+
 def build_lstm(lstm_size, num_layers, batch_size, keep_prob):
-    '''
+    ''' 
     构建lstm层
 
     keep_prob
@@ -64,21 +133,28 @@ def build_lstm(lstm_size, num_layers, batch_size, keep_prob):
     batch_size: batch_size
 
     '''
-    def get_drop():
+    def get_drop_layer(lstm_size):
+
         # 构建一个基本lstm单元
-        lstm = tf.nn.rnn_cell.BasicLSTMCell(lstm_size)
+        lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
+
         # 添加dropout
-        drop = tf.nn.rnn_cell.DropoutWrapper(lstm, output_keep_prob=keep_prob)
+        drop = tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob)
         return drop
+
     # 堆叠
-    cell = tf.nn.rnn_cell.MultiRNNCell([get_drop() for _ in range(num_layers)])
+    cell = tf.contrib.rnn.MultiRNNCell([get_drop_layer(lstm_size) for _ in range(num_layers)])
     initial_state = cell.zero_state(batch_size, tf.float32)
 
     return cell, initial_state
 
 
+# ## 3.3 输出层
+
+# In[11]:
+
 def build_output(lstm_output, in_size, out_size):
-    '''
+    ''' 
     构造输出层
 
     lstm_output: lstm层的输出结果
@@ -89,7 +165,7 @@ def build_output(lstm_output, in_size, out_size):
 
     # 将lstm的输出按照列concate，例如[[1,2,3],[7,8,9]],
     # tf.concat的结果是[1,2,3,7,8,9]
-    seq_output = tf.concat(lstm_output, 1)  # tf.concat(concat_dim, values)
+    seq_output = tf.concat(lstm_output, axis=1)  # tf.concat(concat_dim, values)
     # reshape
     x = tf.reshape(seq_output, [-1, in_size])
 
@@ -106,6 +182,10 @@ def build_output(lstm_output, in_size, out_size):
 
     return out, logits
 
+
+# ## 3.4 训练误差计算
+
+# In[12]:
 
 def build_loss(logits, targets, lstm_size, num_classes):
     '''
@@ -129,8 +209,13 @@ def build_loss(logits, targets, lstm_size, num_classes):
     return loss
 
 
+# ## 3.5 Optimizer
+# 我们知道RNN会遇到梯度爆炸（gradients exploding）和梯度弥散（gradients disappearing)的问题。LSTM解决了梯度弥散的问题，但是gradient仍然可能会爆炸，因此我们采用gradient clippling的方式来防止梯度爆炸。即通过设置一个阈值，当gradients超过这个阈值时，就将它重置为阈值大小，这就保证了梯度不会变得很大。
+
+# In[13]:
+
 def build_optimizer(loss, learning_rate, grad_clip):
-    '''
+    ''' 
     构造Optimizer
 
     loss: 损失
@@ -146,6 +231,11 @@ def build_optimizer(loss, learning_rate, grad_clip):
 
     return optimizer
 
+
+# ## 3.6 模型组合
+# 使用tf.nn.dynamic_run来运行RNN序列
+
+# In[14]:
 
 class CharRNN:
     def __init__(self, num_classes, batch_size=64, num_steps=50,
@@ -180,12 +270,29 @@ class CharRNN:
         self.loss = build_loss(self.logits, self.targets, lstm_size, num_classes)
         self.optimizer = build_optimizer(self.loss, learning_rate, grad_clip)
 
-batch_size = 100         # Sequences per batch
-num_steps = 100          # Number of sequence steps per batch
-lstm_size = 512         # Size of hidden layers in LSTMs
-num_layers = 3          # Number of LSTM layers
-learning_rate = 0.001    # Learning rate
-keep_prob = 0.5         # Dropout keep probability
+
+# # 4 模型训练
+
+# ### 参数设置
+# 在模型训练之前，我们首先初始化一些参数，我们的参数主要有：
+# 
+# - num_seqs: 单个batch中序列的个数
+# - num_steps: 单个序列中字符数目
+# - lstm_size: 隐层结点个数
+# - num_layers: LSTM层个数
+# - learning_rate: 学习率
+# - keep_prob: dropout层中保留结点比例
+
+# In[15]:
+
+batch_size = 100  # Sequences per batch
+num_steps = 100  # Number of sequence steps per batch
+lstm_size = 512  # Size of hidden layers in LSTMs
+num_layers = 1  # Number of LSTM layers
+learning_rate = 0.001  # Learning rate
+keep_prob = 0.5  # Dropout keep probability
+
+# In[16]:
 
 epochs = 20
 # 每n轮进行一次变量保存
@@ -195,40 +302,52 @@ model = CharRNN(len(vocab), batch_size=batch_size, num_steps=num_steps,
                 lstm_size=lstm_size, num_layers=num_layers,
                 learning_rate=learning_rate)
 
-# saver = tf.train.Saver(max_to_keep=100)
-# with tf.Session() as sess:
-#     sess.run(tf.global_variables_initializer())
-#
-#     counter = 0
-#     for e in range(epochs):
-#         # Train network
-#         new_state = sess.run(model.initial_state)
-#         loss = 0
-#         for x, y in get_batches(encoded, batch_size, num_steps):
-#             counter += 1
-#             start = time.time()
-#             feed = {model.inputs: x,
-#                     model.targets: y,
-#                     model.keep_prob: keep_prob,
-#                     model.initial_state: new_state}
-#             batch_loss, new_state, _ = sess.run([model.loss,
-#                                                  model.final_state,
-#                                                  model.optimizer],
-#                                                 feed_dict=feed)
-#
-#             end = time.time()
-#             # control the print lines
-#             if counter % 100 == 0:
-#                 print('轮数: {}/{}... '.format(e + 1, epochs),
-#                       '训练步数: {}... '.format(counter),
-#                       '训练误差: {:.4f}... '.format(batch_loss),
-#                       '{:.4f} sec/batch'.format((end - start)))
-#
-#             if (counter % save_every_n == 0):
-#                 saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
-#
-#     saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
+saver = tf.train.Saver(max_to_keep=100)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
 
+    counter = 0
+    for e in range(epochs):
+        # Train network
+        new_state = sess.run(model.initial_state)
+        loss = 0
+        for x, y in get_batches(encoded, batch_size, num_steps):
+            counter += 1
+            start = time.time()
+            feed = {model.inputs: x,
+                    model.targets: y,
+                    model.keep_prob: keep_prob,
+                    model.initial_state: new_state}
+            batch_loss, new_state, _ = sess.run([model.loss,
+                                                 model.final_state,
+                                                 model.optimizer],
+                                                feed_dict=feed)
+
+            end = time.time()
+            # control the print lines
+            if counter % 100 == 0:
+                print('轮数: {}/{}... '.format(e + 1, epochs),
+                      '训练步数: {}... '.format(counter),
+                      '训练误差: {:.4f}... '.format(batch_loss),
+                      '{:.4f} sec/batch'.format((end - start)))
+
+            if (counter % save_every_n == 0):
+                saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
+
+    saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
+
+# In[17]:
+
+# 查看checkpoints
+tf.train.get_checkpoint_state('checkpoints')
+
+
+# # 5 文本生成
+# 现在我们可以基于我们的训练参数进行文本的生成。当我们输入一个字符时，LSTM会预测下一个字符，我们再将新的字符进行输入，这样能不断的循环下去生成本文。
+# 
+# 为了减少噪音，每次的预测值我会选择最可能的前5个进行随机选择，比如输入h，预测结果概率最大的前五个为[o,e,i,u,b]，我们将随机从这五个中挑选一个作为新的字符，让过程加入随机因素会减少一些噪音的生成。
+
+# In[18]:
 
 def pick_top_n(preds, vocab_size, top_n=5):
     """
@@ -247,6 +366,8 @@ def pick_top_n(preds, vocab_size, top_n=5):
     c = np.random.choice(vocab_size, 1, p=p)[0]
     return c
 
+
+# In[19]:
 
 def sample(checkpoint, n_samples, lstm_size, vocab_size, prime="The "):
     """
@@ -295,22 +416,71 @@ def sample(checkpoint, n_samples, lstm_size, vocab_size, prime="The "):
 
     return ''.join(samples)
 
+
+# Here, pass in the path to a checkpoint and sample from the network.
+
+# In[20]:
+
 tf.train.latest_checkpoint('checkpoints')
+
+# In[26]:
+
+# 选用最终的训练参数作为输入进行文本生成
+checkpoint = tf.train.latest_checkpoint('checkpoints')
+samp = sample(checkpoint, 2000, lstm_size, len(vocab), prime="The")
+print(samp)
+
+# In[22]:
 
 checkpoint = 'checkpoints/i200_l512.ckpt'
 samp = sample(checkpoint, 1000, lstm_size, len(vocab), prime="Far")
-with open("final.txt", "a+") as f:
-    print(samp)
-    f.write(samp)
+print(samp)
+
+# In[23]:
 
 checkpoint = 'checkpoints/i1000_l512.ckpt'
 samp = sample(checkpoint, 1000, lstm_size, len(vocab), prime="Far")
-with open("final.txt", "a+") as f:
-    print(samp)
-    f.write(samp)
+print(samp)
 
-checkpoint = tf.train.latest_checkpoint('checkpoints')
-samp = sample(checkpoint, 1000, lstm_size, len(vocab), prime="The")
-with open("final.txt", "a+") as f:
-    print(samp)
-    f.write(samp)
+# In[24]:
+
+checkpoint = 'checkpoints/i2000_l512.ckpt'
+samp = sample(checkpoint, 1000, lstm_size, len(vocab), prime="Far")
+print(samp)
+
+# LAYER2:
+# There have to be disheable to him; but she walked up again, he had ashamed that the master was thinking of this since
+# he saw a start in a long, having done. She spoke obviously to say anything
+# was in the doctor's face. The discass, the bedroom sent feet in the
+# mustic and supper. Still was so the peace and with her soul. And
+# when she was intradicting, but the same still moment were standing at
+# the posestees, the bearliss of his househell, that had been displessed
+# to see him as she was so already, and so than he had not been seriously
+# satcassive the subjuct, when he could not be all tirided."
+#
+# "Oh, we're the disagrieation."
+#
+# "Yes, in his expression of talking the same assisting the concert of a
+# carriage," thought Kitty in anything, but his horse could not love her
+# seeing him. The cheire himself, who had taken out the chair and asked a
+# suppre and marriage, true his hand and howed her thoughts and the pinches
+# of the presence at the same delight, his eyes said it was in the world,
+# they were all the peasants, had still to go.
+#
+# "You wouldn't believe it!"
+#
+# "I have not come for her in the peasant and the same terms, the steps of
+# making me still more. They're settled to me all the properly."
+#
+# He tried to try to his brother of the servants in the sola of an old
+# last, as though there would be sure that what he had been their family,
+# hasicisation. It said that the servants was asking her, and the
+# consuction he did not know and how it was a self-consideration of
+# her husband's household and so more only all the clearly the place was
+# an attances of supporting the such carriages. And he, that in the
+# crowd of the mind it was the people of the moments who was the conviction
+# himself was struggling in all the conversation to them in the same
+# tribe and taking out and should be in seem, and,, and he had soothed
+# her, that he had been discussing with her, he saw a sense, but he did
+# not think and stay. They were not trying to discornound the manner,
+# and shaked one shall been sore what had say to
